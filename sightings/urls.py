989fff8273ed8dafb3907205926f08eb62c95fc0
jw3991 <80441242+jw3991@users.noreply.github.com>
2021-04-11 21:22:02 +0800

    Update urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,7 +5,7 @@ from . import views
 apps_name = 'sightings'
 urlpatterns = [
     path('', views.squirrels_list, name='squirrels_list'),
+    path('stats/', views.stats, name='stats'),
+    path('add/', views.add, name='add'),
     path('<str:Squirrel_ID>/', views.update, name='update'),
-    path('stats', views.stats, name='stats'),
-    path('add', views.add, name='add'),
 ]
